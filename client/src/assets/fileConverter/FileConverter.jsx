ed4cb49bb3c75c1091c14b9380a3200fef18c491
Cjay Salgado <fionasalgado098@gmail.com>
2026-05-12 03:23:19 +0800

    File upload modal | Incomplete

--- client/src/assets/fileConverter/FileConverter.jsx
+++ client/src/assets/fileConverter/FileConverter.jsx
@@ -1,26 +1,37 @@
 import './fileConverterStyles.css'
 import conversionTypes from './conversionTypes.json'
+import { useState, useRef } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faX, faFile, faCloud } from '@fortawesome/free-solid-svg-icons'
 
 function FileConverter(){
+
+    const [showFileModal, setShowFileModal] = useState(false)
+    const [useFileType, setUseFileType] = useState("file")
+
     return(
         <div className="page file-converter-root">
             {
                 conversionTypes.map((type, index) => (
-                    <FileTypeSelect fileType={type.name} logoSrc={type.logoSrc} supportedFormat={type.supportedFormat}/>
+                    <FileTypeSelect fileType={type.name} logoSrc={type.logoSrc} supportedFormat={type.supportedFormat} showFileModal={showFileModal} setShowFileModal={setShowFileModal} setUseFileType={setUseFileType}/>
                 ))
+            }
+            {
+                showFileModal && <UploadFileModal useFileType={useFileType} showFileModal={showFileModal} setShowFileModal={setShowFileModal}/>
             }
         </div>
     )
 }
 
-function FileTypeSelect( {fileType, logoSrc, supportedFormat} ){
+function FileTypeSelect( {fileType, logoSrc, supportedFormat, showFileModal, setShowFileModal, setUseFileType} ){
     function onFileTypeSelect(fileType){
-        alert(`Converting as ${fileType}`)
+        setUseFileType(fileType)
+        setShowFileModal(!showFileModal)
     }
 
     return(
         <div className="file-type-select" id={fileType} onClick={() => onFileTypeSelect(fileType)}>
-            <div className='left flex-row align-items-center'>
+            <div className='left flex-row-centerx'>
                 <div className='file-type-logo-container'>
                     <img className='file-type-logo' src={logoSrc} alt={`${fileType} logo`}/>            
                 </div>
@@ -40,6 +51,75 @@ function FileTypeSelect( {fileType, logoSrc, supportedFormat} ){
                         )
                     })    
                 }</p>
+            </div>
+        </div>
+    )
+}
+
+function UploadFileModal({useFileType, showFileModal, setShowFileModal}){
+    const inputFileRef = useRef(null);
+    const [isDragging, setIsDragging] = useState(false)
+    const [fileName, setFileName] = useState("")
+
+    function onCloseModalClick(showFileModal, setShowFileModal){
+        setShowFileModal(!showFileModal)
+    }    
+
+    function handleDragOver(e){
+        e.preventDefault()
+        setIsDragging(true)
+    }
+
+    function handleDragLeave(){
+        setIsDragging(false)
+    }
+
+    function handleDrop(e){
+        e.preventDefault()
+        setIsDragging(false)
+
+        const file = e.dataTransfer.files[0]
+
+        if (file) {
+            setFileName(file.name)
+            console.log(file)
+        }
+    }
+
+    function handleFileChange(e){
+        const file = e.target.files[0]
+
+        if(file){
+            setFileName(file.name)
+            console.log(file)
+        }
+    }
+
+    return(
+        <div className='upload-file-modal-container'>
+            <div className='upload-file-modal'>
+                <input
+                    ref={inputFileRef}
+                    type='file'
+                    hidden
+                    onChange={handleFileChange}
+                />
+                <div className='head flex-column-centery'>
+                    <p className='upload-modal-head-label'>Upload File</p>
+                    <p className='upload-modal-head-description'>Select or upload {useFileType}</p>
+                    <FontAwesomeIcon icon={faX} className='upload-modal-close' onClick={() => {onCloseModalClick(showFileModal, setShowFileModal)}}/>
+                </div>
+                <div className='main'
+                    onDragOver={handleDragOver}
+                    onDragLeave={handleDragLeave}
+                    onDrop={handleDrop}
+                >
+                    <FontAwesomeIcon icon={faCloud} className='upload-modal-cloud-icon' />
+                    <p>Choose a file or drag & drop it here</p>
+                    <button
+                        onClick={() => inputFileRef.current.click()}
+                    >Browse file</button>
+                </div>
             </div>
         </div>
     )
